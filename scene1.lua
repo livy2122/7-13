a3e4b31b8b9fd75f63142727c67df0be4421f27a
livy2122 <47640791+livy2122@users.noreply.github.com>
2019-05-10 12:57:49 -0400

    Add files via upload

---
+++ scene1.lua
@@ -0,0 +1,126 @@
+
+   local physics = require( "physics" )
+
+         physics.start()
+         physics.setGravity( 0, 25 )
+      
+
+
+
+local composer = require( "composer" )
+ 
+local scene = composer.newScene()
+ 
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+ 
+-- create()
+function scene:create( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs when the scene is first created but has not yet appeared on screen
+ 
+end
+ 
+ 
+ -- local function showScene1 ()
+ --       local options = {
+ --             effect = "fade",
+ --             time = 500
+ --    }         
+ --   composer.gotoScene( "scene1", options)
+ -- end
+-- show()
+
+-- show()
+function scene:show( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+   
+
+      
+      local map = display.newImageRect( "Sky.jpg", 2100,2100 )
+            map.x = 900
+            map.y = display.contentHeight - 1050
+                
+
+                local theGround1 = display.newImage( "land.png" )
+theGround1.x = 520
+theGround1.y = display.contentHeight
+theGround1.id = "the ground"
+physics.addBody( theGround1, "static", { 
+    friction = 0.5, 
+    bounce = 0.3 
+    } ) 
+
+
+local theCharacter = display.newImage( "Idle.png" )
+theCharacter.x = display.contentCenterX - 200
+theCharacter.y = display.contentCenterY
+theCharacter.id = "the character"
+physics.addBody( theCharacter, "dynamic", { 
+    density = 3.0, 
+    friction = 0.5, 
+    bounce = 0.3 
+    } )
+theCharacter.isFixedRotation = true
+
+
+        -- Code here runs when the scene is still off screen (but is about to come on screen)
+       local MyText = display.newText( "Scene 1", 380, 640, native.systemFont, 48)
+       MyText:setFillColor(1, 1, 1)
+       sceneGroup:insert(MyText)
+
+    elseif ( phase == "did" ) then
+        -- Code here runs when the scene is entirely on screen
+ 
+    end
+end
+ 
+ 
+-- hide()
+function scene:hide( event )
+ 
+    local sceneGroup = self.view
+    local phase = event.phase
+ 
+    if ( phase == "will" ) then
+        -- Code here runs when the scene is on screen (but is about to go off screen)
+ 
+    elseif ( phase == "did" ) then
+        -- Code here runs immediately after the scene goes entirely off screen
+           timer.performWithDelay(3000, showScene1)
+    end
+end
+ 
+ 
+-- destroy()
+function scene:destroy( event )
+ 
+    local sceneGroup = self.view
+    -- Code here runs prior to the removal of scene's view
+ 
+end
+ 
+ 
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+ 
+return scene
